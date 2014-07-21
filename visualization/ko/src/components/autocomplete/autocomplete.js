6f57ef729297a0be69411f691ff84471028a8bd0
Nuria Ruiz <nuria@wikimedia.org>
2014-07-21 12:07:37 +0200

    Adding another source dataset for autocomplete

--- visualization/ko/src/components/autocomplete/autocomplete.js
+++ visualization/ko/src/components/autocomplete/autocomplete.js
@@ -4,16 +4,24 @@ define(['knockout', 'text!./autocomplete.html','fetcher', 'typeahead'], function
   
   /** Represents an autocomplete option **/
   //TODO move option model to its own file
-  function Option(data){
+  function ProjectOption(data){
       // no need for these to be observables as they are fixed values
       this.name = data.name;
       this.languages = data.languages;
       this.description = data.description;
   }
   
+  function LanguageOption(data){
+      // no need for these to be observables as they are fixed values
+      this.name = data.name;
+      this.projects = data.projects;
+      this.description = data.description;
+  }
+  
   function Autocomplete(params) {
     this.message = ko.observable(params.message);
-    this.options = ko.observableArray([]);
+    this.projectOptions = ko.observableArray([]);
+    this.languageOptions = ko.observableArray([]);
     this.testing = ko.observable(true);
     this.displaySuboptions = ko.observable(false);
     this.selectedOption = ko.observable();
@@ -27,10 +35,11 @@ define(['knockout', 'text!./autocomplete.html','fetcher', 'typeahead'], function
         
         update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
             
-            var data = bindingContext.$data.options();
+            var projectOptions = bindingContext.$data.projectOptions();
+            var languageOptions = bindingContext.$data.languageOptions();
         
             // to destroy typeaheads: $('.typeahead').typeahead('destroy');           
-            if (data.length >0) {
+            if (projectOptions.length >0 && languageOptions.length>0) {
                 
                 // Data is an array of option objects       
                 var substringMatcher = function(data) {
@@ -58,29 +67,44 @@ define(['knockout', 'text!./autocomplete.html','fetcher', 'typeahead'], function
                 };
                 
                 //TODO need to use ko templates here
-                var templates = {
-                     suggestion: function (data) {
+                var projectTemplate = {
+                    header: " <h5> &nbsp; Available Projects</h5>",
+                    suggestion: function (data) {
                             return "<span class=\"text-muted\">"+data.name+"<span class=\"footnote\"> "+data.description+"</span> </span>";
                      }
-                 }
+                 };
+                 
+                 //TODO need to use ko templates here
+                 var languageTemplate = {
+                     header: "<h5> &nbsp; Available Languages </h5>",
+                     suggestion: function (data) {
+                             return "<span class=\"text-muted\">"+data.name+"<span class=\"footnote\"> "+data.description+"</span> </span>";
+                      }
+                  };
+                 
                 $('.typeahead').typeahead({
                     hint: false,
                     highlight: true,
-                    minLength: 2
+                    minLength: 1
                     },
                     {
                       name: 'projects',
                       displayKey: 'value',
-                      valueKey:'name',
-                      templates: templates,
-                      source: substringMatcher(data)
-                  
-                }).bind('typeahead:selected', self.displaySecondLevel.bind(self));
-            }
-        }
-    };
-  
-  }
+                      templates: projectTemplate,
+                      source: substringMatcher(projectOptions)      
+                    },
+                    {
+                      name: 'language',
+                      displayKey: 'language',
+                      templates: languageTemplate,
+                      source: substringMatcher(languageOptions)      
+                    }
+                
+                ).bind('typeahead:selected', self.displaySecondLevel.bind(self));
+            } //end update 
+        } // end projectAutocomplete
+    }
+  } //end constructor
   
   Autocomplete.prototype.getBrowsingOptions = function() {
       /**
@@ -99,25 +123,39 @@ define(['knockout', 'text!./autocomplete.html','fetcher', 'typeahead'], function
       //Fecher is getting loaded but the way module is written, fetcher
       //ends up on global scope.
       //TODO rewrite fetcher in a require_js friendly format
-      window.Fetcher.json('../../config/autocompleteOptions.json', function(json) {
-          this.options(ko.utils.arrayMap(json, function (data) { return new Option(data); }));
+      window.Fetcher.json('../../config/autocompleteOptionsByProject.json', function(json) {
+          this.projectOptions(ko.utils.arrayMap(json, function (data) { return new ProjectOption(data); }));
+      
+      }.bind(this));
+      
+      window.Fetcher.json('../../config/autocompleteOptionsByLanguage.json', function(json) {
+          this.languageOptions(ko.utils.arrayMap(json, function (data) { return new LanguageOption(data); }));
       
       }.bind(this));
   }
   
   Autocomplete.prototype.displaySecondLevel = function(event, suggestion, datasetName){
+     
       this.displaySuboptions(true);
       this.selectedOption(suggestion.name);
-      var languages = suggestion.languages;
       var suboptions = [];
-      for (var p in languages) {
-          if(languages.hasOwnProperty(p)){
-              suboptions.push({option:p});
+      
+      if (datasetName === "projects") {
+          var languages = suggestion.languages;
+          for (var p in languages) {
+              if(languages.hasOwnProperty(p)){
+                  suboptions.push({option:p});
+              }
+          }
+      } else {
+          var projects = suggestion.projects;
+          for (var p in projects) {
+              if(projects.hasOwnProperty(p)){
+                  suboptions.push({option:p});
+              }
           }
       }
       this.suboptions(suboptions);
-      
-      
   }
   
   Autocomplete.prototype.hideSecondLevel = function(data,event){
