20b8632438c9311af33349b888e28981f4b15b60
Nuria Ruiz <nuria@wikimedia.org>
2015-09-09 07:29:35 -0700

    Adding click binding and prettier buttons

--- visualization/ko/src/components/game-page/game.js
+++ visualization/ko/src/components/game-page/game.js
@@ -4,7 +4,20 @@ define(["knockout", "text!./game.html"], function (ko, gameTemplate) {
 	/**
 	 * Generates 5 letters of alphabet at random
 	 **/
-	function shakeIt() {
+	function _shakeIt() {
+
+	}
+
+	function GameViewModel(route) {
+		this.message = ko.observable('Welcome!');
+		this.letters = ko.observableArray();
+		this.showGameBtn = ko.observable(false);
+	}
+
+
+
+	GameViewModel.prototype.shakeIt = function () {
+		console.log("clicked");
 		var values = new Array();
 		var holder = new Int8Array(5);
 		window.crypto.getRandomValues(holder);
@@ -22,30 +35,8 @@ define(["knockout", "text!./game.html"], function (ko, gameTemplate) {
 
 		}
 
-		return values;
-	}
-
-	function GameViewModel(route) {
-		this.message = ko.observable('Welcome!');
-		this.letters = ko.observableArray();
-
-		// add handler for "shake it" to button
-		var btn = document.getElementById("shake-it");
-
-		function shakeitWrapper() {
-			var randomLetters = shakeIt();
-			this.letters(randomLetters);
-
-		}
-
-		btn.addEventListener('click', shakeitWrapper.bind(this))
-
-	}
-
-
-
-	GameViewModel.prototype.shakeIt = function () {
-		this.message('You invoked doSomething() on the viewmodel.');
+		this.letters(values);
+		this.showGameBtn(true);
 	};
 
 	return {
